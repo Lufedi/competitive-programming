e599e57056339feeda8969714ad42cd36cb3ba53
Felipe Diaz <pipexir@gmail.com>
2019-05-04 16:48:17 -0500

    regex match rec and iterative

--- Interviewbit/dp/RegexMatch.py
+++ Interviewbit/dp/RegexMatch.py
@@ -1,24 +1,52 @@
 class Solution:
-    def __init__(self):
-        self.A = None
-        self.B = None
     def isMatch(self, A,B):
-        self.A = A
-        self.B = B
-        return 1 if match(0,0) else 0
-    def match(i,j):
-        if i == len(self.A) and j==len(self.B):
+        if not len(A) and not len(B):
+            return 1
+        if not len(B):
+            return 0
+        if len(B) - B.count('*') > len(A):
+            return 0
+        A = "@" + A
+        B = "@" + B
+        dp = [[None for j in range(len(B))] for i in range(len(A))]
+        for i in range(len(A)):
+            for j in range(len(B)):
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                else:
+                    if A[i] == B[j] or  B[j] == "?":
+                            dp[i][j] = dp[i-1][j-1] if i > 0 and j > 0 else False
+                    else:
+                        if B[j] == "*":
+                            dp[i][j] = dp[i][j] or ( dp[i-1][j] if i > 0 else False)
+                            dp[i][j] = dp[i][j] or (dp[i-1][j-1] if i > 0 and j > 0 else False)
+                            dp[i][j] = dp[i][j] or ( dp[i][j-1] if j >0 else False)
+        return 1  if dp[len(A)-1][len(B)-1] else 0        
+        #return 1 if self.match(len(A),len(B)) else 0
+    """def match(self, i,j):
+        if i == 0 and j==0:
             return True
+        elif self.memo[i][j] != None:
+            return self.memo[i][j]
         else:
+            res = None
             if self.A[i] == self.B[j]:
-                return self.match(i+1,j+1)
+                res = self.match(i-1,j-1) if i > 0 and j > 0 else False
             else:
                 if self.B[j]=="?":
-                    return self.match(i+1,j+1)
+                    res = self.match(i-1,j-1) if i > 0 and j > 0 else False
                 elif self.B[j]=="*":
-                    return self.match(i+1,j) or self.match(i+1,j+1)
+                    res = res or ( self.match(i-1,j) if i > 0 else False)
+                    res = res or (self.match(i-1,j-1) if i > 0 and j > 0 else False)
+                    res = res or ( self.match(i, j-1) if j >0 else False)
                 else:
-                    return False
+                    res = False
+            if i >=0  and j >= 0:
+                self.memo[i][j] = res 
+            return res
+    """
 
 def main():
-    s = 
+    s = Solution()
+    print(s.isMatch("aa","aa**") )
+main()
